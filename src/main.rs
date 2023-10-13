67a382491c745d8c839b181c78e26517e11fdfeb
Simon Jakobsson <simme.jakobsson@gmail.com>
2023-10-13 10:31:24 +0200

    init rust

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+// Function to add two numbers
+fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_add() {
+        assert_eq!(add(2, 3), 5);
+    }
+}
